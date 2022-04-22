4c2e9f044f3cde61bd59d71215ff361f19f31948
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-21 17:14:09 -0700

    Removed code to take care of edge cases. Will attempt stretch work for this assignment later if I have time.

--- fetcher.js
+++ fetcher.js
@@ -18,16 +18,6 @@ request(userInput[0], (error, response, body) => {
       console.log("Failed to write to file ", err);
       return;
     }
-    if (userInput[1]) {
-      process.stdout.write(`The file ${userInput[1]} already exists. Do you want to overwrite it - Y/N? `);
-      if (process.stdin.on("Y")) {
-        console.log("Existing file overwritten..");
-        console.log(`Downloaded and saved ${body.length} bytes to ${userInput[1]}`);
-      } else if (process.stdin.on("N")) {
-        process.exit();
-      } else if (!process.stdin.on("Y") || process.stdin.on("N")) {
-        console.log("Invalid entry. Enter either Y or N");
-      }
-    } 
+    console.log(`Downloaded and saved ${body.length} bytes to ${userInput[1]}`);
   });
 });
